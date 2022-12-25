15494c1b7168406bb8ee316ffa32e790350322af
edwintantawi <edwintantawi.workspace@gmail.com>
2022-12-25 23:52:51 +0700

    feat: add user domain dto, provider, repository, and usecase

---
+++ internal/user/domain/dto.go
@@ -0,0 +1,16 @@
+package domain
+
+import "github.com/edwintantawi/taskit/internal/user/domain/entity"
+
+// CreateUserIn represents the input of user creation.
+type CreateUserIn struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// CreateUserOut represents the output of user creation.
+type CreateUserOut struct {
+	ID    entity.UserID `json:"id"`
+	Email string        `json:"email"`
+}
