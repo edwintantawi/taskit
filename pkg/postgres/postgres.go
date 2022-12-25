e1f7cf7f499d9926cc67b4a704ca0fabcfbeece5
edwintantawi <edwintantawi.workspace@gmail.com>
2022-12-25 23:50:21 +0700

    feat: add postgres package for postgres database

---
+++ pkg/postgres/postgres.go
@@ -0,0 +1,21 @@
+package postgres
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const Driver = "postgres"
+
+func New(dsn string) *sql.DB {
+	db, err := sql.Open(Driver, dsn)
+	if err != nil {
+		log.Fatalf("Failed open postgres connection: %v", err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Failed to ping postgres database: %v", err)
+	}
+	return db
+}
