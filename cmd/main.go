3a7a684d1639dd1be72325a0bdd75b42ad6110ea
edwintantawi <edwintantawi.workspace@gmail.com>
2022-12-28 16:24:34 +0700

    feat: add task router

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,9 @@ import (
 	authMiddleware "github.com/edwintantawi/taskit/internal/auth/delivery/http/middleware"
 	authRepository "github.com/edwintantawi/taskit/internal/auth/repository"
 	authUsecase "github.com/edwintantawi/taskit/internal/auth/usecase"
+	taskHTTPHandler "github.com/edwintantawi/taskit/internal/task/delivery/http"
+	taskRepository "github.com/edwintantawi/taskit/internal/task/repository"
+	taskUsecase "github.com/edwintantawi/taskit/internal/task/usecase"
 	userHTTPHandler "github.com/edwintantawi/taskit/internal/user/delivery/http"
 	userRepository "github.com/edwintantawi/taskit/internal/user/repository"
 	userUsecase "github.com/edwintantawi/taskit/internal/user/usecase"
@@ -45,12 +48,17 @@ func main() {
 	authHTTPHandler := authHTTPHandler.New(authUsecase)
 	authMiddleware := authMiddleware.New(jwtProvider)
 
+	taskRepository := taskRepository.New(db, idProvider)
+	taskUsecase := taskUsecase.New(taskRepository)
+	taskHTTPHandler := taskHTTPHandler.New(taskUsecase)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
 	// public routes
 	r.Group(func(r chi.Router) {
 		r.Post("/api/users", userHTTPHandler.Post)
+
 		r.Post("/api/authentications", authHTTPHandler.Post)
 		r.Put("/api/authentications", authHTTPHandler.Put)
 	})
@@ -58,8 +66,11 @@ func main() {
 	// private routes (need authentication)
 	r.Group(func(r chi.Router) {
 		r.Use(authMiddleware)
+
 		r.Get("/api/authentications", authHTTPHandler.Get)
 		r.Delete("/api/authentications", authHTTPHandler.Delete)
+
+		r.Post("/api/tasks", taskHTTPHandler.Post)
 	})
 
 	log.Printf("Server running at %s", cfg.ServerAddr)
