d3aa4d71ce21e1617001abc62b969f39e1fbffd2
edwintantawi <edwintantawi.workspace@gmail.com>
2023-01-01 22:08:08 +0700

    chore: add path aliases for /src

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: new URL('./src/', import.meta.url).pathname,
+      },
+    ],
+  },
 });
