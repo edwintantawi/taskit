d10fdba1e334f5eb57a32ce660c07e4cbd7a942d
edwintantawi <edwintantawi.workspace@gmail.com>
2023-01-01 22:00:47 +0700

    feat: install and setup tailwindcss

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import './common/styles/index.css';
 import { App, AppProvider } from './app';
 
 const rootElement = document.getElementById('root') as HTMLElement;
