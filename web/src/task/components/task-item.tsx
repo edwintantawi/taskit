db5435766b2136040f6803fe32c2ec05703b0b91
edwintantawi <edwintantawi.workspace@gmail.com>
2023-01-02 21:35:15 +0700

    feat: task action only visible on hover

--- web/src/task/components/task-item.tsx
+++ web/src/task/components/task-item.tsx
@@ -16,7 +16,7 @@ export function TaskItem() {
           <CalendarIcon className="h-4" /> 1 Jan 2023 13:00
         </p>
 
-        <div className=" absolute top-0 right-0 flex h-10 text-sm text-gray-500 group-hover:visible">
+        <div className="invisible absolute top-0 right-0 flex h-10 text-sm text-gray-500 group-hover:visible">
           <div className="w-10  bg-gradient-to-l from-white to-transparent" />
           <div className="flex items-center gap-1 bg-white pl-3">
             <button className="rounded-md p-1 hover:bg-gray-100">
