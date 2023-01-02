d7b4f5ca8e9f1a5452087ca9a053d296ba81b0fb
edwintantawi <edwintantawi.workspace@gmail.com>
2023-01-02 16:36:59 +0700

    feat: add home page route

--- web/src/app/app-router.tsx
+++ web/src/app/app-router.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { SignInPage, SignUpPage } from '~/authentication/pages';
+import { HomePage } from '~/common/pages';
 
 export function AppRouter() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
       <Route path="/authentications">
         <Route path="sign-in" element={<SignInPage />} />
         <Route path="sign-up" element={<SignUpPage />} />
