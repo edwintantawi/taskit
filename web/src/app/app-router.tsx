e71d817296d4145e11578cc3409fdc4128dd689d
edwintantawi <edwintantawi.workspace@gmail.com>
2023-01-02 18:13:40 +0700

    feat: add route to /app

--- web/src/app/app-router.tsx
+++ web/src/app/app-router.tsx
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 
 import { SignInPage, SignUpPage } from '~/authentication/pages';
 import { HomePage } from '~/common/pages';
+import { TaskPage } from '~/task/pages';
 
 export function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/app" element={<TaskPage />} />
       <Route path="/authentications">
         <Route path="sign-in" element={<SignInPage />} />
         <Route path="sign-up" element={<SignUpPage />} />
