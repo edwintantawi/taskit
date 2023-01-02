ae238a62005a39ef860757a47f364c6c20d0c642
edwintantawi <edwintantawi.workspace@gmail.com>
2023-01-02 19:28:02 +0700

    feat: add icon components

--- web/src/common/components/icons/check-icon.tsx
+++ web/src/common/components/icons/check-icon.tsx
@@ -11,7 +11,7 @@ export function CheckIcon({ className }: CheckIconProps) {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={2.5}
+      strokeWidth={1.5}
       stroke="currentColor"
       className={twMerge('aspect-square h-6', className)}
     >
--- web/src/common/components/icons/index.ts
+++ web/src/common/components/icons/index.ts
@@ -1,3 +1,5 @@
 export * from './calendar-icon';
 export * from './check-icon';
 export * from './plus-icon';
+export * from './edit-icon';
+export * from './delete-icon';
