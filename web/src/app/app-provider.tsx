316ca59315dee2281f070ba23cff1eecc2664634
edwintantawi <edwintantawi.workspace@gmail.com>
2023-01-03 21:46:16 +0700

    feat: add react-query client provider

--- web/src/app/app-provider.tsx
+++ web/src/app/app-provider.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
+
+import { AuthContextProvider } from '~/authentication/contexts';
 
 interface AppProviderProps {
   children?: React.ReactNode;
 }
 
+const queryClient = new QueryClient();
+
 // Setup all application provider
 export function AppProvider({ children }: AppProviderProps) {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>{children}</AuthContextProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
 }
