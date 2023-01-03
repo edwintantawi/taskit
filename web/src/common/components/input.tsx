26385c24e2a9c2c2db612131cee8bd92da16ed98
edwintantawi <edwintantawi.workspace@gmail.com>
2023-01-03 21:29:25 +0700

    feat: add forward ref

--- web/src/common/components/input.tsx
+++ web/src/common/components/input.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { twMerge } from 'tailwind-merge';
 
-export function Input({
-  className,
-  ...props
-}: React.InputHTMLAttributes<HTMLInputElement>) {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: string;
+}
+
+export const Input = React.forwardRef(function Input(
+  { className, error, ...props }: InputProps,
+  ref: React.ForwardedRef<HTMLInputElement>
+) {
   const baseClassLists =
     'w-full rounded-md border py-3 px-5 focus:outline-gray-900 placeholder:text-sm md:placeholder:text-base';
   const classLists = twMerge(baseClassLists, className);
 
-  return <input {...props} className={classLists} />;
-}
+  return (
+    <div>
+      <input {...props} className={classLists} ref={ref} />
+      {error && <span className="m-2 block text-sm text-red-500">{error}</span>}
+    </div>
+  );
+});
