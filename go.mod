103d7249ddf78bd13efc0423d72a72b1b247f81c
edwintantawi <edwintantawi.workspace@gmail.com>
2023-01-04 18:03:02 +0700

    feat: add postgres auto migration
    split postgres dsn into individual config

--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/go-chi/chi/v5 v5.0.8
 	github.com/go-chi/cors v1.2.1
 	github.com/golang-jwt/jwt/v4 v4.4.3
+	github.com/golang-migrate/migrate v3.5.4+incompatible
+	github.com/golang-migrate/migrate/v4 v4.15.2
 	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.7
@@ -16,10 +18,12 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -2,15 +2,40 @@ package postgres
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	_ "github.com/lib/pq"
 )
+
+type Config struct {
+	Port     string
+	Host     string
+	DB       string
+	User     string
+	Password string
+	SSLMode  string
+}
 
 const Driver = "postgres"
 
 // New create new postgres connection.
-func New(dsn string) *sql.DB {
+func New(config *Config) (*sql.DB, func(autoMigrate bool) error) {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		config.User,
+		config.Password,
+		config.Host,
+		config.Port,
+		config.DB,
+		config.SSLMode,
+	)
+
 	db, err := sql.Open(Driver, dsn)
 	if err != nil {
 		log.Fatalf("Failed open postgres connection: %v", err)
@@ -18,5 +43,29 @@ func New(dsn string) *sql.DB {
 	if err := db.Ping(); err != nil {
 		log.Fatalf("Failed to ping postgres database: %v", err)
 	}
-	return db
+
+	migrate := func(autoMigrate bool) error {
+		if !autoMigrate {
+			return nil
+		}
+		iofsDriver, err := iofs.New(os.DirFS("migrations"), ".")
+		if err != nil {
+			return err
+		}
+		migrator, err := migrate.NewWithSourceInstance("iofs", iofsDriver, dsn)
+		if err != nil {
+			return err
+		}
+		err = migrator.Up()
+		if errors.Is(err, migrate.ErrNoChange) {
+			log.Println("Migration no change, nothing to migrate")
+			return nil
+		} else if err != nil {
+			return err
+		}
+		log.Println("Successfully migrate database")
+		return nil
+	}
+
+	return db, migrate
 }
