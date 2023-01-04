086829b9a031a5bf440733d65d3540ed003c838b
edwintantawi <edwintantawi.workspace@gmail.com>
2023-01-04 08:30:48 +0700

    fix: bad experience with refetchOnWindowFocus in auth-profile

--- web/src/authentication/hooks/use-auth-profile.tsx
+++ web/src/authentication/hooks/use-auth-profile.tsx
@@ -16,6 +16,7 @@ export function useAuthProfile() {
 
   const query = useQuery('auth-profile', AuthenticationService.getAuthProfile, {
     retry: false,
+    refetchOnWindowFocus: false,
     onSuccess,
   });
 
