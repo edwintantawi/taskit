13e80add052beb2fca3fadeb5ac2f7ac0b2e2252
edwintantawi <edwintantawi.workspace@gmail.com>
2023-01-13 10:43:59 +0700

    feat: add project repository find all by user id

--- internal/project/repository/repository.go
+++ internal/project/repository/repository.go
@@ -13,10 +13,12 @@ type Repository struct {
 	idProvider domain.IDProvider
 }
 
+// New create a new project repository.
 func New(db *sql.DB, idProvider domain.IDProvider) Repository {
 	return Repository{db: db, idProvider: idProvider}
 }
 
+// Store save a new project to database.
 func (r *Repository) Store(ctx context.Context, p *entity.Project) (entity.ProjectID, error) {
 	id := entity.ProjectID(r.idProvider.Generate())
 	q := `INSERT INTO projects (id, user_id, title) VALUES ($1, $2, $3)`
@@ -25,4 +27,28 @@ func (r *Repository) Store(ctx context.Context, p *entity.Project) (entity.Proje
 		return "", err
 	}
 	return id, nil
+}
+
+func (r *Repository) FindAllByUserID(ctx context.Context, userID entity.UserID) ([]entity.Project, error) {
+	q := `SELECT id, user_id, title, created_at, updated_at FROM projects WHERE user_id = $1`
+	rows, err := r.db.QueryContext(ctx, q, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	projects := make([]entity.Project, 0)
+	for rows.Next() {
+		var project entity.Project
+		err := rows.Scan(&project.ID, &project.UserID, &project.Title, &project.CreatedAt, &project.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+		projects = append(projects, project)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return projects, nil
 }
--- internal/project/repository/repostiory_test.go
+++ internal/project/repository/repostiory_test.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"regexp"
 	"testing"
 
@@ -95,6 +96,158 @@ func (s *ProjectRepositoryTestSuite) TestCreate() {
 
 			s.Equal(t.expected.projectID, userID)
 			s.Equal(t.expected.err, err)
+		})
+	}
+}
+
+func (s *ProjectRepositoryTestSuite) TestFindAllByUserID() {
+	type args struct {
+		ctx    context.Context
+		userID entity.UserID
+	}
+	type expected struct {
+		projects      []entity.Project
+		allowAnyError bool
+		err           error
+	}
+	tests := []struct {
+		name     string
+		args     args
+		expected expected
+		setup    func(d *dependency)
+	}{
+		{
+			name: "it should return error when database fail to query",
+			args: args{
+				ctx:    context.Background(),
+				userID: "user-xxxxx",
+			},
+			expected: expected{
+				projects: nil,
+				err:      test.ErrDatabase,
+			},
+			setup: func(d *dependency) {
+				d.mockDB.ExpectQuery(regexp.QuoteMeta(`SELECT id, user_id, title, created_at, updated_at FROM projects WHERE user_id = $1`)).
+					WithArgs("user-xxxxx").
+					WillReturnError(test.ErrDatabase)
+			},
+		},
+		{
+			name: "it should return error when database rows fail to scan",
+			args: args{
+				ctx:    context.Background(),
+				userID: "user-xxxxx",
+			},
+			expected: expected{
+				projects:      nil,
+				allowAnyError: true,
+				err:           errors.New("anything"),
+			},
+			setup: func(d *dependency) {
+				mockRow := sqlmock.NewRows([]string{"id", "user_id", "title", "created_at", "updated_at"}).
+					AddRow(nil, "user-xxxxx", "project_title", test.TimeBeforeNow, test.TimeBeforeNow)
+
+				d.mockDB.ExpectQuery(regexp.QuoteMeta(`SELECT id, user_id, title, created_at, updated_at FROM projects WHERE user_id = $1`)).
+					WithArgs("user-xxxxx").
+					WillReturnRows(mockRow)
+			},
+		},
+		{
+			name: "it should return error when database rows error",
+			args: args{
+				ctx:    context.Background(),
+				userID: "user-xxxxx",
+			},
+			expected: expected{
+				projects: nil,
+				err:      test.ErrRows,
+			},
+			setup: func(d *dependency) {
+				mockRow := sqlmock.NewRows([]string{"id", "user_id", "title", "created_at", "updated_at"}).
+					AddRow("project-xxxxx", "user-xxxxx", "project_title_x", test.TimeBeforeNow, test.TimeBeforeNow).
+					AddRow("project-yyyyy", "user-xxxxx", "project_title_y", test.TimeBeforeNow, test.TimeBeforeNow).
+					RowError(1, test.ErrRows)
+
+				d.mockDB.ExpectQuery(regexp.QuoteMeta(`SELECT id, user_id, title, created_at, updated_at FROM projects WHERE user_id = $1`)).
+					WithArgs("user-xxxxx").
+					WillReturnRows(mockRow)
+			},
+		},
+		{
+			name: "it should return error nil and empty slice task when successfully query with no tasks",
+			args: args{
+				ctx:    context.Background(),
+				userID: "user-xxxxx",
+			},
+			expected: expected{
+				projects: []entity.Project{},
+				err:      nil,
+			},
+			setup: func(d *dependency) {
+				mockRow := sqlmock.NewRows([]string{"id", "user_id", "title", "created_at", "updated_at"})
+				d.mockDB.ExpectQuery(regexp.QuoteMeta(`SELECT id, user_id, title, created_at, updated_at FROM projects WHERE user_id = $1`)).
+					WithArgs("user-xxxxx").
+					WillReturnRows(mockRow)
+			},
+		},
+		{
+			name: "it should return error nil and all project when successfully query",
+			args: args{
+				ctx:    context.Background(),
+				userID: "user-xxxxx",
+			},
+			expected: expected{
+				projects: []entity.Project{
+					{
+						ID:        "project-xxxxx",
+						UserID:    "user-xxxxx",
+						Title:     "project_title_x",
+						CreatedAt: test.TimeBeforeNow,
+						UpdatedAt: test.TimeBeforeNow,
+					},
+					{
+						ID:        "project-yyyyy",
+						UserID:    "user-xxxxx",
+						Title:     "project_title_y",
+						CreatedAt: test.TimeBeforeNow,
+						UpdatedAt: test.TimeBeforeNow,
+					},
+				},
+				err: nil,
+			},
+			setup: func(d *dependency) {
+				mockRow := sqlmock.NewRows([]string{"id", "user_id", "title", "created_at", "updated_at"}).
+					AddRow("project-xxxxx", "user-xxxxx", "project_title_x", test.TimeBeforeNow, test.TimeBeforeNow).
+					AddRow("project-yyyyy", "user-xxxxx", "project_title_y", test.TimeBeforeNow, test.TimeBeforeNow)
+
+				d.mockDB.ExpectQuery(regexp.QuoteMeta(`SELECT id, user_id, title, created_at, updated_at FROM projects WHERE user_id = $1`)).
+					WithArgs("user-xxxxx").
+					WillReturnRows(mockRow)
+			},
+		},
+	}
+
+	for _, t := range tests {
+		s.Run(t.name, func() {
+			db, mockDB, err := sqlmock.New()
+			if err != nil {
+				s.FailNow("an error '%s' was not expected when opening a database mock connection", err)
+			}
+
+			d := &dependency{
+				mockDB: mockDB,
+			}
+			t.setup(d)
+
+			repository := New(db, d.idProvider)
+			tasks, err := repository.FindAllByUserID(t.args.ctx, t.args.userID)
+
+			if t.expected.allowAnyError {
+				s.Error(err)
+			} else {
+				s.Equal(t.expected.err, err)
+			}
+			s.Equal(t.expected.projects, tasks)
 		})
 	}
 }
