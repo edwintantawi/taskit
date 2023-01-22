36fcaed055928b80b154a575901d6fee5babc28b
edwintantawi <edwintantawi.workspace@gmail.com>
2023-01-22 22:53:03 +0700

    refactor!: clean up from scratch

--- migrations/000001_create_users_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-  id          VARCHAR(64)   PRIMARY KEY,
-  name        VARCHAR(255)  NOT NULL,
-  email       VARCHAR(255)  NOT NULL UNIQUE,
-  password    TEXT          NOT NULL,
-  created_at  TIMESTAMP     NOT NULL DEFAULT NOW(),
-  updated_at  TIMESTAMP     NOT NULL DEFAULT NOW()
-);
--- migrations/000002_create_authentications_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE authentications (
-  id          VARCHAR(64)   PRIMARY KEY,
-  user_id     VARCHAR(255)  NOT NULL,
-  token       TEXT          NOT NULL,
-  expires_at  TIMESTAMP     NOT NULL,
-
-  CONSTRAINT fk_authentications_users FOREIGN KEY(user_id) REFERENCES users(id)
-);
